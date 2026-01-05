e581b9ee4613bc79e577e629aa7e795d4bcca396
pedroLima019 <pedroliveiralima21@gmail.com>
2026-01-05 20:37:59 -0300

    aprendendo sobre classes e como resolver problemas com classes

--- programacaoOrientadaObjetos/src/main/java/exercicio01.java
+++ programacaoOrientadaObjetos/src/main/java/exercicio01.java
@@ -1,28 +1,34 @@
+import entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
+import  entities.Triangle;
 
 public class exercicio01 {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        Triangle x , y ;
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("Descreva as medidas do triangulo X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        /* acessando atributo */
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Descreva as medidas do triangulo Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        double p = (x.a + x.b + x.b) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
         if (areaX > areaY) {
             System.out.println("O triangulo maior é o X");
