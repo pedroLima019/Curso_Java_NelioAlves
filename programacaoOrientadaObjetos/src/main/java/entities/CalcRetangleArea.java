7f2103c99a36d60ada523cebbcaadfdaf9ac59c4
pedroLima019 <pedroliveiralima21@gmail.com>
2026-01-13 21:27:46 -0300

    adicionando exercicio03 para fixação de conteúdo

---
+++ programacaoOrientadaObjetos/src/main/java/entities/CalcRetangleArea.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class CalcRetangleArea {
+
+    private double width;
+    private double height;
+
+    public CalcRetangleArea(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt((width * width) + (height * height));
+    }
+
+    @Override
+    public String toString() {
+        return "AREA: " + area() +
+                "\nPERIMETRO: " + perimeter() +
+                "\nDIAGONAL: " + diagonal();
+    }
+}
+
---
+++ programacaoOrientadaObjetos/src/main/java/exercicios03.java
@@ -0,0 +1,8 @@
+import entities.CalcRetangleArea;
+
+public class exercicios03 {
+     public static  void  main(String[] args){
+         CalcRetangleArea retangle = new CalcRetangleArea(3 , 4);
+         System.out.println(retangle);
+     }
+}
